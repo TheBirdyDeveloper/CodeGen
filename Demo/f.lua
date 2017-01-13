d6f596d5a037afb0608f84430238e16e927c69c8
JeanBobNNinja <mathieu.menuet@live.fr>
2017-01-13 13:28:06 +0100

    Dossier Demo

---
+++ Demo/f.lua
@@ -0,0 +1,13 @@
+require('luaLib/print')
+function F1(X1)
+   X1="a"
+print(type(X1))
+return X1
+end
+
+local values = {F1(PARAMS)}
+
+for k, v in pairs( values ) do
+   print(k,printUniq(v))
+end
+
